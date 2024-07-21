0d9937a496eefd815294c00464e651ea90638ae9
Titu Sahu <study@Titus-MacBook-Air.local>
2024-07-21 13:27:03 +0530

    Updated resume_app.py to include AdSense script

--- resume_app.py
+++ resume_app.py
@@ -7,10 +7,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
 
-# Inject the meta tag into the HTML
+# Inject the AdSense script into the HTML
 st.markdown(
     """
-    <meta name="google-adsense-account" content="ca-pub-3075073753451202">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3075073753451202"
+            crossorigin="anonymous"></script>
     """,
     unsafe_allow_html=True
 )
